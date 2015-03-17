e8d7220462cd0ea5f65985c95c19f430fd59ac43
J. TURNER <turner.j6@gmail.com>
2015-03-16 17:23:42 -0700

    Establish files.

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,31 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+    render :index
+  end
+
+  def new
+    @list = List.new
+    render :new
+  end
+
+  def create
+    @list = List.new(params[:list])
+    if @list.save
+      redirect_to lists_path
+    else
+      render :new
+    end
+  end
+
+
+  def show
+    @list = List.find(params[:id])
+    render :show
+  end
+
+  def edit
+    @list = List.find(params[:id])
+    render :edit
+  end
+end
